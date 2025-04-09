73db62576a8bd8c7ee62116b5ec28f761478793d
muriloags <mags.sect@gmail.com>
2025-04-09 14:03:37 -0300

    Initial configuration of ReailHub with PostgreSQL and MongoDB

---
+++ src/main/java/com/retailhub/RetailhubApplication.java
@@ -0,0 +1,13 @@
+package com.retailhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retailhub/RetailhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
